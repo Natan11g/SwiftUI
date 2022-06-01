e6ff851d52af8546073ec1a3752e9bb283b97928
Natan11g <natan.gluszko@natwest.com>
2022-06-01 23:23:38 +0100

    test

---
+++ SwiftUI experimenting/SwiftUI experimenting/SwiftUI_experimentingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_experimentingApp.swift
+//  SwiftUI experimenting
+//
+//  Created by Natan Gluszko on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_experimentingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
