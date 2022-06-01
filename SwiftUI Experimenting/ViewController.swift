29e8db3b0180f6eb9254ee946b176f16476ec5aa
Natan11g <natan.gluszko@natwest.com>
2022-06-01 22:21:45 +0100

    Initial Commit

---
+++ SwiftUI Experimenting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI Experimenting
+//
+//  Created by Natan Gluszko on 01/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
